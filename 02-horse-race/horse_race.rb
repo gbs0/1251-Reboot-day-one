b27495d5443ad9d328b6bfc7517b42053d2ecad1
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2023-04-24 14:35:08 -0300

    Adicionado challenge 2

---
+++ 02-horse-race/horse_race.rb
@@ -0,0 +1,21 @@
+# Escreva um programa em Ruby, que simule uma corrida de cavalos
+
+# • Quais são as regras?
+# • Uma corrida de cavalos é definida por uma lista de animais competidores;
+# • Cada jogador pode escolher em qual cavalo apostar;
+# • Ao final da corrida, resta apenas um vencedor (cavalo);
+# • Devemos verificar se o jogador ganhou ou não a aposta;
+
+# Definindo o passo a passo:
+# 1. Imprimir uma mensagem de boas vindas ao jogador
+
+# 2. Mostrar ao jogador, em quais cavalos ele pode apostar
+
+# 3. Guardamos qual foi o competidor escolhido pelo apostador
+
+# 4. Iniciarmos a corrida de cavalos!
+
+# 5. Ao final da corrida, precisamos descobrir qual o vencedor
+
+# 6. Conferir se a aposta do jogador foi a ganhadora/perdedora
+
