eeebbc9495cea56b3bb31a758929a1ca13479872
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2023-04-24 15:51:46 -0300

    automatted commit

---
+++ 03-Insta-card/interface.rb
@@ -0,0 +1,11 @@
+# Escrever um programa que simule uma pequena loja de venda de items
+
+# 1. Imprimir na tela a mensagem de boas vindas!
+
+# 2. Mostrar na tela, quais são os itens disponiveis na loja
+
+# 3. Perguntar ao usuário, qual o item que ele quer.
+# 3.1 Se tivermos o item, vamos adicionar no carrinho
+# 3.2 Se NÃO tivermos o item, mostraremos uma mensagem de item indisponivel
+
+# 4. 
