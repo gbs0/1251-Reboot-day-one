e21208b7169685c21edc09c3995ca14054e970c2
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2023-04-24 11:49:32 -0300

    automatted commit

---
+++ 01-calculator/interface.rb
@@ -0,0 +1,16 @@
+# Escreva um programa em ruby, que sirva como calculadora!
+
+# 1. Dizer olá p/ o usuário!
+
+# 2. Perguntar ao usuário qual é o primeiro número
+
+# 3. Guardar o primeiro número do usuário em uma váriavel
+
+# 4. Perguntar ao usuário, qual o segundo número
+
+# 5. Guardar o segundo número do user em uma váriavel
+
+# 6. Perguntar ao usuário qual a operação [ +, -, *, / ]
+
+# 7. Imprimir na tela o resultado da operação do usuário
+
